8a105467cbbcd8a9ce7c3b993ba8e140102c890d
TryAgain-Web <davinbowen09@gmail.com>
2025-09-29 01:10:58 -0500

    Update to home page and product page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  scr = '../assets/Images/Front.jpg'
 
+  Lemon = '../assets/Images/Cake/lemon.jpg'
+  name = 'Lemon Cake'
+
+  Cho = '../assets/Images/Juices/chocolate.jpg'
+  name2 = 'Chocolate Drink'
+
+  RNR = '../assets/Images/Icream/RNR.jpeg'
+  name3 = 'Ice-Cream'
   constructor() { }
 
   ngOnInit(): void {
--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,11 +1,111 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Food {
+  image: string
+  name: string
+}
+
+
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+
+  Juices: Food[] = [
+    {
+      image : '../assets/Images/Juices/chocolate.jpg',
+      name : 'chocolate drink',
+    },
+    {
+    image : '../assets/Images/Juices/Creeper.jpg',
+    name : 'Creeper Rum cream punch',
+    },
+    {
+      image: '../assets/Images/Juices/Cucumber.jpg',
+      name : 'Cucumber Juice',
+    },
+     {
+      image : '../assets/Images/Juices/Pineapple.jpg',
+      name : 'Pineapple Juice',
+    },
+     {
+      image : '../assets/Images/Juices/Strawberry.jpg',
+      name : 'Strawberry fresca juice',
+    },
+     {
+      image : '../assets/Images/Juices/Watermelon.jpg',
+      name : 'Watermelon juice',
+    },
+  ]
+
+  cakes : Food[] = [
+    {
+      image : '../assets/Images/Cake/Cheesecakes.jpg',
+      name : 'Cheesecake',
+    },
+    {
+      image :'../assets/Images/Cake/chocolate.jpg' ,
+      name : 'chocolate cake',
+    },
+    {
+      image :'../assets/Images/Cake/lemon.jpg' ,
+      name : 'chocolate cake',
+    },
+    {
+      image :'../assets/Images/Cake/red.jpg' ,
+      name : 'red velvet',
+    },
+    {
+      image :'../assets/Images/Cake/strawberry.jpg' ,
+      name :'strawberry shortcake',
+    },
+    {
+      image :'../assets/Images/Cake/vanilla.jpeg',
+      name : 'vanilla slice',
+    },
+  ]
+
+  ices: Food[] = [
+    {
+      image : '../assets/Images/Icream/chocolate.jpg',
+      name : 'chocolate ice-cream',
+    },
+    {
+      image :'../assets/Images/Icream/CNC.jpg' ,
+      name : 'cookie n cream ice-cream',
+    },
+    {
+      image :'../assets/Images/Icream/Mango.jpeg' ,
+      name : 'mango ice-cream',
+    },
+    {
+      image :'../assets/Images/Icream/RNR.jpeg' ,
+      name : 'rum n raisin ice-cream',
+    },
+    {
+      image :'../assets/Images/Icream/strawberry.jpg' ,
+      name :'strawberry ice-cream',
+    },
+    {
+      image :'../assets/Images/Icream/vanilla.jpg',
+      name : 'vanilla ice-cream',
+    },
+  ]
+
+  mousses: Food[] = [
+    {
+      image:'../assets/Images/Mousse/Chocolate.jpeg' ,
+      name : 'Chocolate mousse',
+    },
+     {
+      image:'../assets/Images/Mousse/lemon.jpg',
+      name : 'lemon mousse',
+    },
+  ]
+
+
 
   constructor() { }
 
